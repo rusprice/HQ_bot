36afb674ea9d8eff9bf1dcac7c720e1e997ab76b
Rus Price <rus@anolet.com>
2024-02-20 20:56:02 -0500

    yes

--- server.js
+++ server.js
@@ -46,6 +46,14 @@ ws.on('message', function message(data) {
 });
 }
 
-for (var i = 0; i < 1500; i++) {
+for (var i = 0; i < 2000; i++) {
     joinGame();
 }
+
+var http = require('http');
+
+
+http.createServer(function (req, res) {
+  res.write('Hello World!'); 
+  res.end();
+}).listen(8080);
